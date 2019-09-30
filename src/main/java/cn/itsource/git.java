d21c3d14846a25e56dd8567c08b5ced93acae3ac
hzw <790009176@qq.com>
2019-09-30 14:52:36 +0800

    员工b 先一步提交

--- src/main/java/cn/itsource/git.java
+++ src/main/java/cn/itsource/git.java
@@ -6,5 +6,7 @@ public class git {
         System.out.println("aaaa");
         System.out.println("bbbbbbb");
         System.out.println("员工B提交第2次");
+        System.out.println("员工B提交分支了哈");
+        System.out.println("员工B先一步提交了代码");
     }
 }
