683e4b2c178f5fc4654f60ced8676b24abf8dde4
hzw <790009176@qq.com>
2019-09-30 14:43:00 +0800

    员工b分支提交

--- src/main/java/cn/itsource/git.java
+++ src/main/java/cn/itsource/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("aaaa");
         System.out.println("bbbbbbb");
         System.out.println("员工B提交第2次");
+        System.out.println("员工B提交分支了哈");
     }
 }
